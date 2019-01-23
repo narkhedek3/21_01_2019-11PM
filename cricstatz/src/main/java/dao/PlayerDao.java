508e7413d3a419deaa4ed11e5fcc17b936c005e0
cdac <cdac@dac22.course.local>
2019-01-23 12:06:18 +0530

    23/01/2019 12.06

---
+++ cricstatz/src/main/java/dao/PlayerDao.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate4.HibernateCallback;
+import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
+import dto.Player;
+
+
+@Repository
+public class PlayerDao {
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
+	
+	public void createTeam(final Player player) {
+		hibernateTemplate.execute(new HibernateCallback<List<Player>>() {
+
+			public List<Player> doInHibernate(Session session) throws HibernateException {
+				Transaction t = session.beginTransaction();
+				session.save(player);
+				t.commit();
+				session.flush();
+				session.close();
+				return null;
+			}
+		});
+	}
+	
+}
